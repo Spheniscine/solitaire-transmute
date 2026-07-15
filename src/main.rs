a6f33adfe9957784be862d50f5ee520e9122eb67
Spheniscine <spheniscine@gmail.com>
2026-07-15 18:34:08 +0800

    import some needed files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use dioxus::prelude::*;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
+
+// altered version of KaTeX_Main to include filled "red" suits
+const KATEX_SUITS: Asset = asset!("/assets/KaTeX_Suits.woff2");
+
+const NISHIKI_ALCHEMY: Asset = asset!("/assets/Nishiki_Alchemy.woff2");
+
+// from https://www.confettijs.org/
+const CONFETTI_JS: Asset = asset!("/assets/confetti.min.js");
+
 const MAIN_CSS: Asset = asset!("/assets/main.css");
 const HEADER_SVG: Asset = asset!("/assets/header.svg");
 
@@ -11,8 +20,38 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
+        document::Link {
+            rel: "preconnect",
+            href: "https://fonts.googleapis.com",
+        }
+        document::Link {
+            rel: "preconnect",
+            href: "https://fonts.gstatic.com",
+            crossorigin: "anonymous",
+        }
+        document::Link {
+            href: "https://fonts.googleapis.com/css2?family=Noto+Color+Emoji&family=Noto+Sans+Symbols+2&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap",
+            rel: "stylesheet",
+        }
+
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
+        document::Style {
+            r#"
+            @font-face {{
+                font-family: KaTeX_Suits;
+                font-style: normal;
+                font-weight: 700;
+                src: url({KATEX_SUITS}) format("woff2");
+            }}
+            @font-face {{
+                font-family: Nishiki_Alchemy;
+                font-style: normal;
+                src: url({NISHIKI_ALCHEMY}) format("woff2");
+            }}    
+            "#,
+        }
+        document::Script { src: CONFETTI_JS }
         Hero {}
 
     }
