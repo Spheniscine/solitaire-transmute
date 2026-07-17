e966feda2f065b63c17f8fe276bdb17ab77c4b67
Spheniscine <spheniscine@gmail.com>
2026-07-17 22:37:42 +0800

    game state

--- src/game/game.rs
+++ src/game/game.rs
@@ -1,4 +1,122 @@
 use std::time::Duration;
 
+use rand::{Rng, seq::SliceRandom};
+use serde::{Deserialize, Serialize};
+use strum::IntoEnumIterator;
+
+use crate::game::{Board, BoardPos, Card, DECK_SIZE, DepotRole, RANKS, Skin, Suit};
+
 pub const ANIMATION_DURATION: Duration = Duration::from_millis(200);
 pub type AnimationKey = u16;
+
+#[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq)]
+pub enum ActionRecord {
+    Move { pos1: BoardPos, pos2: BoardPos, rev: bool },
+    Combine { card1: Card, card2: Card },
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Default)]
+pub enum ScreenState {
+    #[default] Game, 
+    Settings, Help,
+}
+
+#[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq)]
+pub struct GameState {
+    pub board: Board,
+    pub deal: Vec<Card>,
+    #[serde(skip)]
+    pub animation_key: AnimationKey, // used for syncing and to provide animator components with cycling keys
+    pub history: Vec<ActionRecord>,
+    pub undo_stack: Vec<usize>,
+    pub already_won: bool,
+    pub num_wins: i32,
+
+    pub screen_state: ScreenState,
+
+    pub allow_undo: bool,
+    pub skin: Skin,
+}
+
+impl GameState {
+    pub fn new_deal(rng: &mut impl Rng) -> Vec<Card> {
+        let mut deck = Vec::with_capacity(DECK_SIZE);
+        for rank in RANKS {
+            for suit in Suit::iter() {
+                deck.push(Card { rank, suit });
+            }
+        }
+
+        deck.shuffle(rng);
+        deck
+    }
+
+    pub fn init() -> Self {
+        let mut res = Self {
+            board: Board::empty(),
+            deal: vec![],
+            animation_key: 0,
+            history: vec![],
+            undo_stack: vec![],
+            already_won: false,
+            num_wins: 0,
+            screen_state: ScreenState::Game,
+            allow_undo: true,
+            skin: Skin::default(),
+        };
+
+        res.new_game();
+        res
+    }
+
+    pub fn new_game(&mut self) {
+        let deal = Self::new_deal(&mut rand::rng());
+        self.board = Board::from_deal(&deal);
+        self.deal = deal;
+        self.history.clear();
+        self.undo_stack.clear();
+        self.already_won = false;
+
+        // if !self.is_busy() { LocalStorage.save_game_state(&self); }
+    }
+
+    pub fn is_busy(&self) -> bool {
+        self.is_acting()
+    }
+
+    pub fn is_acting(&self) -> bool {
+        !self.board.animation_acts.is_empty()
+    }
+
+    pub fn undo_possible(&self) -> bool {
+        self.allow_undo && !self.undo_stack.is_empty()
+    }
+
+    fn do_move_raw(&mut self, pos1: BoardPos, pos2: BoardPos, rev: bool) {
+        self.board.do_move(pos1, pos2, rev);
+        self.history.push(ActionRecord::Move { pos1, pos2, rev })
+    }
+
+    pub fn can_stack(&self, back: Card, front: Card) -> bool {
+        (back.suit != front.suit || back.suit == Suit::Wild) && 
+        back.rank.abs_diff(front.rank) == 1
+    }
+
+    pub fn can_select(&self, pos: BoardPos) -> bool {
+        let depot = pos.depot_index;
+        let ord = pos.card_index;
+
+        if ord >= self.board.depots[depot].len() {
+            return false;
+        }
+        let slice = &self.board.depots[depot][ord..];
+
+        let Some(role) = DepotRole::role(depot) else { return false };
+        match role {
+            DepotRole::Tableau => slice.windows(2).all(|w| self.can_stack(w[0], w[1])),
+            DepotRole::Foundation => false,
+            DepotRole::EngineIn => slice.len() <= 1,
+            DepotRole::EngineOut => slice.len() <= 1,
+        }
+    }
+}
